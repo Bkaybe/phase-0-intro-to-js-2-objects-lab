03a4958a3af86eab02644d84b84cca56c11a5692
Bethuel Khisa <sunguribethuel@gmail.com>
2024-06-29 18:46:39 +0300

    Adds object manipulation functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+  name: "Zakayo",
+  streetAddress: "11th Street",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newObj = { ...obj };
+
+  newObj[key] = value;
+
+  return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  const newObj = { ...obj };
+
+  const deleted = delete newObj[key];
+
+  return deleted;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  delete obj[key];
+
+  return obj;
+}
